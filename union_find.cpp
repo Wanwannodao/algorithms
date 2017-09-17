5c7d28201c997a814d37284774adb0b7336610ad
Wanwannodao <lavilabyrinth@yahoo.co.jp>
2017-09-17 19:54:53 +0900

    Union-Find, POJ 1182

---
+++ union_find.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+typedef long long ll;
+
+#define FOR(i, n) for (int i=0;i<n;++i)
+#define FOR2(i, s, n) for (int i=s;i<n;++i)
+
+class UnionFind {
+public:
+    vector<int> p, r; // parent, rank
+
+    UnionFind(int n) {
+        // MakeSet
+        p.resize(n); r.resize(n);
+        fill(p.begin(), p.end(), -1);
+        fill(r.begin(), r.end(), 0);   
+    }
+
+    int find(int x) {
+        if (p[x] == -1) return x;
+        else { // path compression
+            p[x] = find(p[x]);
+            return p[x];
+        }
+    }
+
+    void unite(int x, int y) {
+        int xr = find(x), yr = find(y);
+        if( xr == yr ) return; 
+        // union by rank
+        if (r[xr] > r[yr]) p[yr] = xr;
+        else if (r[xr] < r[yr]) p[xr] = yr;
+        else if (xr != yr) { p[yr] = xr; ++r[xr]; } // xr.rank == yr.rank && xr in A and yr in A' A!=A'
+    }
+
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
+};
+
+
+/* Ex: POJ 1182 Food Chain */
+int main()
+{
+    ll n, k;
+    cin >> n >> k;
+
+    // x   : x as an element of A
+    // x+n : x as an element of B
+    // x+2n: x as an element of C
+    UnionFind uf(n+3);
+    
+    ll cnt = 0;
+
+    // construct three pattern of "consistent" sets
+    FOR(i,k) {
+        ll d,x,y;
+        cin >> d >> x >> y;
+
+        if (x>=n || y>=n || x < 0 || y < 0){ cnt++; continue; }
+
+        if (d==1) {
+            // one of inconsistent pattern
+            if ( uf.same(x,y+n) || uf.same(x, y+2*n) ) cnt++; // x in A, y in B or x in A, y in C
+            else FOR(j,3) uf.unite(x+i*n, y+i*n);
+        } else if(d==2) {
+            // one of inconsistent pattern
+            if ( uf.same(x, y) || uf.same(x, y+2*n) ) cnt++; // x in A, y in A or x in A, y in C
+            else { // three consistent patterns
+                uf.unite(x, y+n);
+                uf.unite(x+n, y+2*n);
+                uf.unite(x+2*n, y);
+            }
+        }
+    }
+    cout << cnt << endl;
+}
