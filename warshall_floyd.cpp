8d872c0a4e731c292b64561e3ca7edd5a85491a8
Wanwannodao <lavilabyrinth@yahoo.co.jp>
2017-09-29 23:12:03 +0900

    Warshall-Floyd Algorithm

--- warshall_floyd.cpp
+++ warshall_floyd.cpp
@@ -3,29 +3,33 @@
 #include <cstring>
 #include <vector>
 #include <numeric>
-
+#include <cstdlib>
+#include <queue>
+#include <stack>
 using namespace std;
 
 typedef long long ll;
+#define pb push_back
 
-#define FOR(i, n) for (int i=0;i<n;++i)
-#define INF(bit) (1 << bit)
+#define rep(i, n) for (int i=0;i<(n);++i)
+#define rep2(i, s, n) for (int i=s;i<(n);++i)
+#define INF (1e9)
 
+/* Warshall-Floyd Algorithm */  
 // O(V^3)
 int main()
 {
-    ll n;
-    cin >> n;
-
-    vector<vector<ll> > d(n, vector<ll>(n, INF(29)));
-
+    ll V,E; cin>>V>>E;
+    vector<vector<ll> > d(V, vector<ll>(V, INF));
+    
     // initialization
-    FOR(i,n) {
-        FOR(j,n) {
-            ll e; cin >> e; d[i][j] = e;
-        }
-    }
-
+    rep(i,V) d[i][i] = 0;
+    rep(i,E) { ll u,v,c; cin>>u>>v>>c; d[u][v] = c; }
     // calc
-    FOR(k,n) FOR(i,n) FOR(j,n) if (d[i][j] > d[i][k] + d[k][j]) d[i][j] = d[i][k] + d[k][j];
+    rep(k,V) rep(i,V) rep(j,V) if (d[i][j] > d[i][k] + d[k][j]) d[i][j] = d[i][k] + d[k][j];
+
+    rep(i,V) {
+        rep(j,V) { if (d[i][j] == INF) cout << "INF"; else cout << d[i][j]; if(j!=V-1) cout << " ";}
+        cout << endl;
+    }
 }
